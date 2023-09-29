1ea65fb695dc1ae7ed085aeed8c46ab478a9c7da
devkingsejong <devkingsejong@gmail.com>
2023-09-29 19:38:28 +0900

    fix typo

--- server/src/main/kotlin/ovpn/closedadmin/server/common/controller/CommonController.kt
+++ server/src/main/kotlin/ovpn/closedadmin/server/common/controller/CommonController.kt
@@ -3,7 +3,7 @@ package ovpn.closedadmin.server.common.controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import ovpn.closedadmin.server.common.dto.CommonResponse
-import ovpn.closedadmin.server.common.exception.NotFoundProblem
+import ovpn.closedadmin.server.common.problem.NotFoundProblem
 
 @RestController
 class CommonController {
