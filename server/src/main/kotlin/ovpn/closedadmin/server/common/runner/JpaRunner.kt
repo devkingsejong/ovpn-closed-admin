ec5def068a846cbdbedb35d128c39899ee8f2c45
devkingsejong <devkingsejong@gmail.com>
2023-10-02 12:37:15 +0900

    feat: Create default Admin user When startup server

---
+++ server/src/main/kotlin/ovpn/closedadmin/server/common/runner/JpaRunner.kt
@@ -0,0 +1,29 @@
+package ovpn.closedadmin.server.common.runner
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.CommandLineRunner
+import org.springframework.stereotype.Component
+import ovpn.closedadmin.server.business.admin.entity.AdminEntity
+import ovpn.closedadmin.server.business.admin.repository.AdminRepository
+import ovpn.closedadmin.server.common.util.Encrypt
+import java.util.*
+
+@Component
+class JpaRunner @Autowired constructor(private var adminRepository: AdminRepository) : CommandLineRunner {
+
+    @Throws(Exception::class)
+    override fun run(vararg args: String) {
+        // create user using Password "test"
+        // password rule like purepassword+salt
+        // TODO: Generate Default user with JVMOps
+        val tempUUID = UUID.randomUUID()
+        val tempUser = AdminEntity(
+            tempUUID,
+            "test2",
+            5,
+            "a2asdf@a2.com",
+            "SHA512\$"+Encrypt.toSHA512("test"+tempUUID.toString())
+        );
+        adminRepository.save(tempUser)
+    }
+}
