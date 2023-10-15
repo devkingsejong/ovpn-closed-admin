cbb20a4ee7d2956858d67844842d285d130e9f44
devkingsejong <devkingsejong@gmail.com>
2023-10-15 15:23:45 +0900

    chore: Add Cors Ignore(localhost:3000)

--- server/src/main/kotlin/ovpn/closedadmin/server/ServerApplication.kt
+++ server/src/main/kotlin/ovpn/closedadmin/server/ServerApplication.kt
@@ -2,9 +2,28 @@ package ovpn.closedadmin.server
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication
-class ServerApplication
+class ServerApplication {
+    // CORS 설정 Bean 추가
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")
+                    .allowedOrigins(
+                        "http://localhost:3000",
+                    )
+                    .allowedMethods("GET", "POST", "PUT", "DELETE")
+                    .allowedHeaders("*")
+                    .allowCredentials(true)
+            }
+        }
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<ServerApplication>(*args)
