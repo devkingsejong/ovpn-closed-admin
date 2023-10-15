700f10ab603272c8cebea7d3451cff09ce82ce7e
devkingsejong <devkingsejong@gmail.com>
2023-10-15 15:59:35 +0900

    feat: Define /vpnuser/list API

--- server/src/main/kotlin/ovpn/closedadmin/server/business/account/controller/VpnUserController.kt
+++ server/src/main/kotlin/ovpn/closedadmin/server/business/account/controller/VpnUserController.kt
@@ -1,12 +1,15 @@
 package ovpn.closedadmin.server.business.account.controller
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import ovpn.closedadmin.server.business.account.dto.CreateVpnUserForm
+import ovpn.closedadmin.server.business.account.dto.VpnUserDto
+import ovpn.closedadmin.server.business.account.service.VpnUserService
 import ovpn.closedadmin.server.business.account.usecase.CreateNewVpnUserWithCreateNewOvpnFile
 import ovpn.closedadmin.server.business.admin.vo.Admin
 import ovpn.closedadmin.server.common.dto.CommonResponse
@@ -18,6 +21,9 @@ class VpnUserController {
     @Autowired
     lateinit var createNewVpnUserWithCreateNewOvpnFile: CreateNewVpnUserWithCreateNewOvpnFile
 
+    @Autowired
+    lateinit var vpnUserService: VpnUserService
+
     @PostMapping("/create")
     fun createNewVpnUser(
         @RequestHeader(name = "Authentication") admin: Admin,
@@ -25,5 +31,10 @@ class VpnUserController {
         ): CommonResponse<String> {
             var ovpnFile = createNewVpnUserWithCreateNewOvpnFile.run(createVpnUserForm)
             return CommonResponse(ovpnFile)
+    }
+
+    @GetMapping("/list")
+    fun getVpnUserList(@RequestHeader(name = "Authentication") admin: Admin): CommonResponse<List<VpnUserDto>> {
+        return CommonResponse(vpnUserService.getVpnUserList(0).map { VpnUserDto(it) })
     }
 }
